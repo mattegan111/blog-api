d555cd9298e3b2008eaf38a357ae95a744e28bd0
mattegan111 <mnegan93@gmail.com>
2021-09-08 09:42:37 +1200

    update comment model

--- models/comment.js
+++ models/comment.js
@@ -6,7 +6,8 @@ const commentSchema = new Schema(
     {
         body: { type:String, required: true },
         username: { type:String, required: true },
-        post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
+        post: { type:Schema.Types.ObjectId, ref: 'Post', required: true },
+        edited: { type:Boolean, default: false, required: true },
         timestamp: { type: Date, default: Date.now, required: true },
     }
 );
