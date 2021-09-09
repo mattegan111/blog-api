ed10ec0c8c4568aa2ef6419e565af21c00a67462
mattegan111 <mnegan93@gmail.com>
2021-09-10 11:51:05 +1200

    adds authentication requirement to specific routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const post_controller = require('../controllers/postController');
 const user_controller = require('../controllers/userController');
 const comment_controller = require('../controllers/commentController');
@@ -20,11 +21,11 @@ router.get('/api/posts', post_controller.read_all_posts);
 // view one post
 router.get('/api/posts/:id', post_controller.read_post);
 // create new post
-router.post('/api/posts/', post_controller.create_post);
+router.post('/api/posts/', passport.authenticate("jwt", { session: false }), post_controller.create_post);
 // update post
-router.put('/api/posts/:id', post_controller.update_post);
+router.put('/api/posts/:id', passport.authenticate("jwt", { session: false }), post_controller.update_post);
 // delete post
-router.delete('/api/posts/:id', post_controller.delete_post);
+router.delete('/api/posts/:id', passport.authenticate("jwt", { session: false }), post_controller.delete_post);
 
 /// COMMENTS ///
 // view one posts comments
@@ -32,9 +33,9 @@ router.get('/api/posts/:postid/comments', comment_controller.read_post_comments)
 // create new comment
 router.post('/api/posts/:postid/comments/', comment_controller.create_comment);
 // update comment
-router.put('/api/posts/:postid/comments/:commentid', comment_controller.update_comment);
+router.put('/api/posts/:postid/comments/:commentid', passport.authenticate("jwt", { session: false }), comment_controller.update_comment);
 // delete comment
-router.delete('/api/posts/:postid/comments/:commentid', comment_controller.delete_comment);
+router.delete('/api/posts/:postid/comments/:commentid', passport.authenticate("jwt", { session: false }), comment_controller.delete_comment);
 
 /// users ///
 // view all users
@@ -42,11 +43,11 @@ router.get('/api/users', user_controller.read_all_users);
 // view one user
 router.get('/api/users/:id', user_controller.read_user);
 // create new user
-router.post('/api/users', user_controller.create_user);
+router.post('/api/users', passport.authenticate("jwt", { session: false }), user_controller.create_user);
 // update user
-router.put('/api/users/:id', user_controller.update_user);
+router.put('/api/users/:id', passport.authenticate("jwt", { session: false }), user_controller.update_user);
 // delete user
-router.delete('/api/users/:id', user_controller.delete_user);
+router.delete('/api/users/:id', passport.authenticate("jwt", { session: false }), user_controller.delete_user);
 // sign in 
 router.post('/api/sign-in', user_controller.user_sign_in);
 // sign out
