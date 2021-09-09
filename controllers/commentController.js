7fc4eef948a87318949f99a679212459641dde41
mattegan111 <mnegan93@gmail.com>
2021-09-10 11:43:37 +1200

    fixes delete_comment function

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -65,24 +65,20 @@ exports.update_comment = [
     }
 ];
 
-exports.delete_comment = [
-    body('body', 'required').trim().isLength({ min: 1, max: 160 }).escape(),
-
-    async (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            res.json({
-                data: req.body,
-                errors: errors.array(),
-            });
-            return;
+exports.delete_comment = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.json({
+            data: req.body,
+            errors: errors.array(),
+        });
+        return;
+    } else {
+        const comment = await Comment.findByIdAndDelete(req.params.commentid);
+        if(!comment) { 
+            res.status(404).json({ log: 'comment not found'}); 
         } else {
-            const comment = await Comment.findByIdAndDelete(req.params.commentid);
-            if(!comment) { 
-                res.status(404).json({ log: 'comment not found'}); 
-            } else {
-                res.status(200).json({ log: "deleted successfully" });
-            }
+            res.status(200).json({ log: "deleted successfully" });
         }
     }
-];
+};
